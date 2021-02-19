75bbde495629a046938893051a66bd7233920ac5
Vitalii <this.azeroth@gmail.com>
2021-02-19 14:56:25 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "spring-boot-terraform-asg"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "My Spring Boot App deployed by Terraform"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westeurope"
+  default     = "East US"
 }
